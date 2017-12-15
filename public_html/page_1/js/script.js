8dacb617058dc4a1bbb02f7b52c1e494a68221e6
Jack Reuter <djreuter45@gmail.com>
2017-12-15 13:38:53 -0700

    added switch statement for checkbox values

--- public_html/page_1/js/script.js
+++ public_html/page_1/js/script.js
@@ -1,13 +1,13 @@
-function addDiv() {
+function addButton() {
   // grab the element that the new div will go after
   var target = document.querySelector('.headings');
   // create the new div & give it an id
-  var div = document.createElement('div');
-  div.setAttribute('id', 'js-div');
+  var button = document.createElement('button');
+  button.setAttribute('id', 'js-button');
   // add content to the div
-  div.innerHTML = 'This was added via JS.  Click to remove';
+  button.innerHTML = 'This was added via JS.  Click to remove';
   // add the div to the DOM
-  target.parentNode.insertBefore(div, target.nextSibling);
+  target.parentNode.insertBefore(button, target.nextSibling);
 
   removeFromDom();
 }
@@ -15,15 +15,63 @@ function addDiv() {
 function addToDom() {
     return new Promise((resolve) => {
         setTimeout(function() {
-          addDiv();
+          addButton();
         }, 3000);
         resolve();
   });
 }
 
 function removeFromDom() {
-  var div = document.querySelector('#js-div');
-  div.addEventListener('click', function() {
-    div.parentNode.removeChild(div);
+  var button = document.querySelector('#js-button');
+  button.addEventListener('click', function() {
+    button.parentNode.removeChild(button);
   });
 }
+
+function hideTable() {
+  document.getElementById('table').style.display = 'none';
+}
+
+// store element in variable
+var button = document.getElementById('submit');
+
+// define event listener function
+var onSubmit = function() {
+  var soccer = document.getElementById('soccer');
+  var basketball = document.getElementById('basketball');
+  var football = document.getElementById('football');
+  switch(true) {
+    case soccer.checked && !basketball.checked && !football.checked:
+      alert('You chose soccer!');
+      break;
+    case basketball.checked && !soccer.checked && !football.checked:
+      alert('You chose basketball!');
+      break;
+    case football.checked && !soccer.checked && !basketball.checked:
+      alert('You chose football!');
+      break;
+    case soccer.checked && basketball.checked && !football.checked:
+      alert('you chose soccer and basketball!');
+      break;
+    case soccer.checked && football.checked && !basketball.checked:
+      alert('you chose soccer and football!');
+      break;
+    case basketball.checked && football.checked && !soccer.checked:
+      alert('you chose basketball and football!');
+      break;
+    case football.checked && soccer.checked && basketball.checked:
+      alert('you like it all!');
+      break;
+    default:
+      alert('please choose one or more sports');
+  }
+  // if(soccer.checked || soccer.checked && basketball.checked) {
+  //   alert('Nice choice!');
+  // } else if(basketball.checked && !soccer.checked && !football.checked) {
+  //   alert('You chose basketball!');
+  // } else if(football.checked == true) {
+  //   alert('Meh.');
+  // }
+}
+// add the event listener
+button.addEventListener('click', onSubmit);
