4d8c5690f179392a464acfafab546ae9d9043117
Jack Reuter <djreuter45@gmail.com>
2017-12-19 11:06:17 -0700

    added jQuery functions to prepend div, change element color, and toggle font

--- public_html/page_2/js/script.js
+++ public_html/page_2/js/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-  
+  console.log('jQuery connected');
+
+  // prepend div above headings
+  setTimeout(function() {
+    var $div = $('<div>', {id: 'jquery-div'});
+    $div.text('this div was prepended with jQuery!');
+    $('.headings').prepend($div);
+  }, 2000);
+
+  // change background color on mouse in
+  $('.headings').mousemove(function() {
+    var $headings = $('.headings');
+    $headings.css('background-color','lightskyblue');
+    $headings.mouseout(function() {
+      $headings.css('background-color', 'lightblue');
+    });
+  });
+
+  // set interval for toggling font
+  setInterval(function() {
+    $('.heading-text').toggleClass('toggledFont');
+  }, 3000);
+
+
+
+
 });
